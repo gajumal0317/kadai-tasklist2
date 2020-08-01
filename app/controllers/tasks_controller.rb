68957012a68b49fb26101f0cdda643ec4e8fe21a
gajumal <kazukig0317@gmail.com>
2020-08-01 06:21:16 +0000

    20200801

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    # before_action :set_task, only: [:show, :edit, :update]
+ 
   def index
-    @tasks = Task.all
+   if logged_in?
+    @task = current_user.tasks.build  # form_with 用
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+   end
   end
 
   def show
@@ -17,12 +21,12 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to root_url
+      flash[:success] = 'Tatsk が正常に投稿されました'
+      redirect_to @task 
     else
-       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render 'toppages/index'
+      render :new
     end
   end
 
@@ -30,7 +34,6 @@ class TasksController < ApplicationController
   end
 
   def update
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -42,16 +45,15 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
     redirect_back(fallback_location: root_path)
   end
   
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  # def set_task
+  #   @task = Task.find(id: params[:id])
+  # end
   
   def task_params
     params.require(:task).permit(:content, :status)
